cff42c5a075b9852bbbe12b0f6792727e698b2ae
juliantoyeo <julianto.yeo@gmail.com>
2021-10-27 04:49:35 +0200

    Created turtle script

---
+++ scripts/turtle_script.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+import turtle
+import time
+import re
+
+file = open("turtle", "r")
+content = file.read()
+file.close()
+lines = content.split('\n')
+
+turtle.setup(width = 1000, height = 1000, startx = 0, starty = 0)
+turtle.speed(100000)
+turtle.degrees()
+
+for step in lines:
+    match = re.search(r'\d+', step)
+    if (match):
+        number = int(match.group(0))
+        if "gauche" in step:
+            turtle.lt(number)
+        elif "droite" in step:
+            turtle.rt(number)
+        elif "Avance" in step:
+            turtle.fd(number)
+        elif "Recule" in step:
+            turtle.bk(number)
+
+time.sleep(10)
